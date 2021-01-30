26883d5735af4f33d9f7ceab4e478a1cfdcf8d09
Your Name <you@example.com>
2021-01-30 16:03:00 +0200

    fix cart mobile

--- client2/src/app/products/products.component.ts
+++ client2/src/app/products/products.component.ts
@@ -78,7 +78,7 @@ export class ProductsComponent implements OnInit {
     .then(res => res.json())
     .then(data => {
       this.categories = data;
-      this.selectedCategory = this.categories[6]._id;
+      this.selectedCategory = this.categories[1]._id;
     }).then(() => {
       fetch(`/api/products/getByCategory/${this.selectedCategory}`)
       .then(res => res.json())
